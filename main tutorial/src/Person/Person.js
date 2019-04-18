98e2eeb91c2c5801510e25226b6e2b6eb7302128
Simon Anderson-Dry <simon.andersondry@therealbuzzgroup.com>
2019-04-18 15:28:03 +0100

    removed assignment 1 and 2, only include main tutorial

---
+++ main tutorial/src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Person.css'
+
+class Person extends React.Component {
+    render() { 
+        return(
+            <div className="Person">
+                <p onClick={this.props.click} >I'm {this.props.name} and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
+}
+
+export default Person;
